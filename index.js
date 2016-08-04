dadad40ae1691116d7c31fca6f19e51083606b62
rockbot <raquel@rckbt.me>
2016-08-04 13:49:52 -0700

    first commit woo

---
+++ index.js
@@ -0,0 +1,102 @@
+"use strict";
+
+/* Caching is tricky, and so deserves explanation.
+ *
+ * The general principles are thus: if we have it in cache, serve it up, even
+ * if it's stale. If there's stale data in the cache, update it for future
+ * requests. Don't hammer the backend if it's stale. We only have to fetch it
+ * once.
+ */
+var P = require('bluebird');
+
+var bole = require('bole');
+var logger = bole("background-refresh-cache");
+var debug = require('debuglog')('background-refresh-cache');
+var EventEmitter = require('events');
+
+var pool = require('@npmcorp/redis-pool');
+
+/**
+ * name: the cache name, for namespacing redis keys
+ * fn: the function to cache
+ * ttl: the cache time in seconds
+ */
+class Cache extends EventEmitter {
+  constructor(name, fn, ttl) {
+    super();
+    this.pending = {};
+    this.fn = fn;
+    this.name = name;
+    this.ttl = ttl;
+  }
+
+  get(key) {
+    const cacheKey = this.name + ":" + key;
+    return pool.withConnection(redis => {
+      debug("checking for %j in cache", key);
+      return redis.getAsync(cacheKey).then(JSON.parse)
+    }).then(cached => {
+      if (!cached) {
+        debug("%j is not in cache, fetching", key);
+        return this.fetch(key);
+      } else if (!cached.fetchedAt) {
+        debug("%j is not data set by this version of background-refresh-cache, re-fetching", key);
+        return this.fetch(key);
+      } else {
+        debug("%j is in cache.", key);
+        cached.fetchedFromCacheAt = Date.now();
+
+        var freshAfter = Date.now() - this.ttl * 1000;
+        if (cached.fetchedAt < freshAfter) {
+          debug("Freshening %j because content fetched at %j is older than %j", key, cached.fetchedAt, freshAfter);
+          this.fetch(key).catch(err => {
+            console.log('error will robinson:', err)
+            if (err.statusCode == 404) {
+              debug("Deleting %j from cache", key);
+              return pool.withConnection(redis => {
+                return redis.delAsync(cacheKey);
+              });
+            } else {
+              throw err;
+            }
+          }).catch(err => logger.error(err))
+        }
+
+        return cached.value;
+      }
+    });
+
+  }
+
+  fetch(key) {
+    const cacheKey = this.name + ":" + String(key);
+    if (!this.pending[key]) {
+      debug("no request for %j is pending, starting one", key);
+      this.pending[key] = P.resolve(this.fn(key)).finally(() => {
+        debug("Removing pending request for %j", key);
+        delete this.pending[key]
+      }).tap(value => {
+        return pool.withConnection(function(redis) {
+          debug("Got fresh content for %j, storing to cache", key);
+          return redis.setAsync(cacheKey, JSON.stringify({
+            fetchedAt: Date.now(),
+             value
+          }));
+        }).then(() => {
+          debug("Content for %j is now in cache", key);
+        }).catch(err => {
+          debug("Error %j", err.message);
+          logger.error(err);
+          return value;
+        });
+      });
+      this.emit('fetch', this.pending[key]);
+    } else {
+      debug("Request for %j is already pending", key);
+    }
+
+    return this.pending[key];
+  }
+}
+
+module.exports = Cache;
---
+++ test/index.js
@@ -0,0 +1,118 @@
+"use strict";
+
+const tap = require('tap');
+const P = require('bluebird');
+
+const requireInject = require('require-inject');
+const redis = require('redis-mock');
+const pool = requireInject.installGlobally.andClearCache('@npmcorp/redis-pool', {
+  redis
+});
+const Cache = require('../');
+
+let numCalled = 0;
+
+let getFoo = function (key) {
+  numCalled++;
+
+  if (key === "forceError") {
+    return P.reject(new Error('kablooey'));
+  } else if (key === "forceNotFound") {
+    return P.reject(Object.assign(new Error('not found'), {statusCode: 404}));
+  }
+
+  return P.resolve({value: 'ohai'});
+};
+
+var cache = new Cache('foo', getFoo, 0.1); 
+
+tap.test('cache instantiates with a name, function, and ttl', t => {
+  t.equals(numCalled, 0);
+  t.equals(cache.ttl, 0.1);
+  t.equals(cache.name, 'foo');
+  t.type(getFoo, 'function');
+  t.done()
+});
+
+tap.test('getting from cache requires a fetch on first try', t => {
+  cache.get('boom').then(cached => {
+    t.equals(numCalled, 1);
+    t.ok(cached);
+    t.equal(cached.value, 'ohai');
+    t.done();
+  });
+});
+
+tap.test('getting from cache does not require a fetch on second try', t => {
+  const doneFetching = deferred();
+  cache.get('boom').then(cached => {
+    t.equals(numCalled, 1);
+    t.ok(cached);
+    t.equal(cached.value, 'ohai');
+    P.delay(200).then(() => {
+      cache.once('fetch', () => {
+        t.equals(numCalled, 2);
+        doneFetching.resolve();
+      });
+      P.join(cache.get('boom').then(cached => {
+      }), doneFetching.promise).then(() => t.done());
+    });
+  });
+});
+
+tap.test("ensure backwards capability after fixing a bug", t => {
+  return pool.withConnection(redis => {
+    const obj = {
+      bang: 'blerg'
+    };
+
+    return redis.setAsync('foo:twiddle', JSON.stringify(obj));
+  })
+    .then(() => cache.get('twiddle'))
+    .then(cached => {
+      t.equals(cached.value, 'ohai');
+      t.equals(numCalled, 3);
+    })
+});
+
+tap.test("delete from cache if not found", t => {
+  return pool.withConnection(redis => {
+    const obj = {
+      value: 'blerg',
+      fetchedAt: 1 // force that fetch!
+    };
+
+    const doneFetching = deferred();
+    cache.once('fetch', doneFetching.resolve);
+
+    return redis.setAsync('foo:forceNotFound', JSON.stringify(obj))
+      .then(() => cache.get('forceNotFound'))
+      .then(cached => doneFetching.promise
+        .catch(err => {
+          t.ok(err)
+          t.equal(err.statusCode, 404)
+        })
+        .then(() => redis.getAsync('foo:forceNotFound')))
+      .then(result => t.notOk(result))
+      
+  });
+});
+
+tap.test("errors are passed through verbatim", t => {
+  return cache.get("forceError")
+    .catch(err => {
+      t.ok(err);
+      t.equals(err.message, "kablooey");
+    });
+});
+
+tap.test('wrapping up...', t => {
+  pool.drainAsync().then(() => pool.destroyAllNow());
+  t.done();
+});
+
+function deferred() {
+  let resolve;
+  let promise = new P((accept, reject) => { resolve = accept });
+  return { promise, resolve };
+}
